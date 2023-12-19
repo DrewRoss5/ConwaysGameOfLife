9c91ab93e4665f9c134002ed6baa4e5b37889d13
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-12-18 23:04:07 -0700

    Allow the user to specify a simulation speed

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,36 @@
     const char* clear = "clear";
 #endif
 
-int main(){
-   
+int main(int argc, char** argv){
+    // determine the desired simulation speed, the time each frame should take in tens of milliseconds 
+    int simulationSpeed = 0;
+    std::string speedStr;
+    std::cout << "What simulation speed would you like? (between 1 and 100, lower is faster)" << std::endl;
+    do{
+        std::getline(std::cin, speedStr); 
+        try{
+            simulationSpeed = std::stoi(speedStr);
+            if (simulationSpeed < 1 || simulationSpeed > 100){
+                std::cout << "Please provide an integer between 1 and 100" << std::endl;
+            }
+        }
+        catch (...){
+            std::cout << "Please provide a valid integer." << std::endl;
+        }
+    }
+    while (simulationSpeed == 0);
+    
     try{
         life::Game game = life::readStateFile("testfile.txt");
         while (game.getActive()){
             system(clear);
             game.displayCells();
             game.updateCells();
-            std::this_thread::sleep_for(std::chrono::milliseconds(34));
+            std::this_thread::sleep_for(std::chrono::milliseconds(simulationSpeed * 10));   
         }
         std::cout << "FINISHED" << std::endl;
     }
     catch (...){
-        std::cout << "Error raised" << std::endl;
+        std::cout << "Error parsing the game state file" << std::endl;
     }
-   
-
 }
