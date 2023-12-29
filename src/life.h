e5c02052da47dfa597962574f7b906c1de7340a4
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-12-28 22:46:39 -0700

    Add directions enumerator as well as the  maxHeight_  and maxWidth_ attributes

--- src/life.h
+++ src/life.h
@@ -7,8 +7,13 @@
 namespace life{
     class Game{
         private:
+            // contant enumerator for the four cardinal direction
+            enum DIRECTIONS_{NORTH, EAST, SOUTH, WEST};
+            // class attributes
             int width_;
             int height_;
+            int maxWidth_;
+            int maxHeight_;
             int overpop_;
             int underpop_;
             int reproduction_;
@@ -17,10 +22,12 @@ namespace life{
             std::vector<std::vector<bool>> cellRows_;
         public:
             Game() : width_(0), height_(0), overpop_(3), underpop_(2), reproduction_(3), generation_(0), active_(false) {} // default constructor
-            Game(int width, int height, std::vector<int> rules, std::vector<std::vector<bool>> cells);
+            Game(int width, int height, int maxWidth, int maxHeight, std::vector<int> rules, std::vector<std::vector<bool>> cells);
             bool getCell(int x, int y);
             void displayCells() const;
             void updateCells();
+            void addColumn(int direction);
+            void addRow(int direction);
             //getter functions
             int getWidth() const {return width_;}
             int getHeight() const {return height_;}
