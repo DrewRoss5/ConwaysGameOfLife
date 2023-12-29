82858aad3346398fa537d8616255848aaacf75b7
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-12-29 11:44:06 -0700

    Fix the default constructor

--- src/life.h
+++ src/life.h
@@ -21,7 +21,7 @@ namespace life{
             bool active_;
             std::vector<std::vector<bool>> cellRows_;
         public:
-            Game() : width_(0), height_(0), overpop_(3), underpop_(2), reproduction_(3), generation_(0), active_(false) {} // default constructor
+            Game() : width_(0), height_(0), maxWidth_(0), maxHeight_(0), overpop_(3), underpop_(2), reproduction_(3), generation_(0), active_(false), cellRows_({}){} // default constructor
             Game(int width, int height, int maxWidth, int maxHeight, std::vector<int> rules, std::vector<std::vector<bool>> cells);
             bool getCell(int x, int y);
             void displayCells() const;
